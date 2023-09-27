3d5838dcbfb77e1efa10fbf23bc17a05b3b8aab5
Yunus Emre MERT <yunusemremert@yandex.com>
2023-09-27 17:38:08 +0300

    ConnectPostgreSQL() method completed

--- configs/database.go
+++ configs/database.go
@@ -1,1 +1,17 @@
 package configs
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectPostgreSQL() *gorm.DB {
+	dsn := EnvPostgreDBUrl()
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
