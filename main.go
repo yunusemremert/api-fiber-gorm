e221df36b086591431692f8f883ecd4a28bbb654
Yunus Emre MERT <yunusemremert@yandex.com>
2023-09-27 19:27:46 +0300

    ConnectPostgreSQL() defined

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	r "api-fiber-gorm/app"
+	"api-fiber-gorm/configs"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+
+	configs.ConnectPostgreSQL()
 
 	r.SetupRoutes(app)
 
